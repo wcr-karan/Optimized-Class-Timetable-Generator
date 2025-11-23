78810e239fe551b9964b2342ae31a806f8567dc4
MOHITKOURAV01 <mohitkourav@MOHITs-MacBook-Pro-2.local>
2025-11-23 18:38:29 +0530

    Update project files

--- frontend/src/pages/Faculty.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Faculty = () => {
-  return <div>Faculty Page</div>;
-};
-
-export default Faculty;
--- frontend/src/services/auth.api.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Departments = () => {
-  return <div>Departments Page</div>;
-};
-
-export default Departments;
